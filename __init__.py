295a639443bfcbe856f5a0be867e8c83d05a68ba
yang0 <yang0@126.com>
2024-12-24 18:49:03 +0800

    撒旦飞洒地方

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from .index import *
 import os
 
-# testaa
+# test
 
-VERSION="1.0.14"
+VERSION="1.0.15"
 GIT_URL="https://github.com/yang0/autotask_autogui"
 NAME="自动化鼠标键盘操作"
 DESCRIPTION="桌面自动化(RPA)插件"
