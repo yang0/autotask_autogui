564df0f65a5fc62f3701fe2bcc450170797f85a1
yang0 <yang0@126.com>
2025-01-12 19:14:49 +0800

    232

--- __init__.py
+++ __init__.py
@@ -4,9 +4,9 @@ import os
 
 # testa啊
 
-VERSION = "1.0.25"
+VERSION = "1.0.26"
 GIT_URL="https://github.com/yang0/autotask_autogui"
 NAME="自动化鼠标键盘操作a"
 DESCRIPTION="桌面自动化(RPA)插件"
 
-TAGS = ["autogui"]
+TAGS = ["test"]
