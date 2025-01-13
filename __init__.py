858546824bd7054ebcdc0e95b1c8d34c82e5141a
yang0 <yang0@126.com>
2025-01-13 09:00:21 +0800

    sdf

--- __init__.py
+++ __init__.py
@@ -4,9 +4,9 @@ import os
 
 # testa啊
 
-VERSION = "1.0.27"
+VERSION = "1.0.28"
 GIT_URL="https://github.com/yang0/autotask_autogui"
-NAME="自动化鼠标键盘操作"
+NAME="自动化鼠标键盘操作啊"
 DESCRIPTION="桌面自动化(RPA)插件"
 
 TAGS = ["test"]
