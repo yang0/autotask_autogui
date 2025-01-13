4b06ba4732de6b9d88d04bb5cbb8fe381f7afc40
yang0 <yang0@126.com>
2025-01-13 16:06:50 +0800

    的风格

--- __init__.py
+++ __init__.py
@@ -4,9 +4,12 @@ import os
 
 # testa啊
 
-VERSION = "1.0.29"
+VERSION = "1.0.30"
 GIT_URL="https://github.com/yang0/autotask_autogui"
 NAME="自动化鼠标键盘操作"
-DESCRIPTION="桌面自动化(RPA)插件"
+DESCRIPTION="""桌面自动化(RPA)插件
+sadf
+asdf
+"""
 
 TAGS = ["test"]
